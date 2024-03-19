c550cb1d03f04dd68dcba7e4c85c93eeb62f2545
Berke <berkesat.dev@gmail.com>
2024-03-20 00:22:19 +0300

    First Commit

--- src/Taximeter.java
+++ src/Taximeter.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Taximeter {
+import java.util.Scanner;
+public class Taximeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter distance traveled: ");
+        double distance = input.nextDouble();
+
+        double taxiFee = 10;
+
+        final double feePerKM = 2.2;
+
+        taxiFee += (feePerKM * distance);
+
+        if (taxiFee < 20) {
+            System.out.println("Taxi fee is 20");
+        }
+        else {
+            System.out.println("Taxi fee is " + taxiFee);
+        }
+
+
+    }
 }
